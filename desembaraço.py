319f16b2ab2c2b6027b0fce204cc23e961ab522f
israelicc <100978658+israelicc@users.noreply.github.com>
2022-03-04 22:09:40 -0300

    Add files via upload

---
+++ desembaraço.py
@@ -0,0 +1,10 @@
+def displayPDF(file):
+    # Opening file from file path
+    with open(r"C:\Users\Adm\Desktop\Desembaraço_Aduaneiro.pdf", "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
